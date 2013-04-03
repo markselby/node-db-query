5a8ce1fbd4157da2b39be7c62860539b842863b9
Mark Selby <mark@writebox.co.uk>
2013-04-03 10:22:33 +0800

    Initial import

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-node-db-query
-=============
+# node-db-query
 
 Create SQL queries programatically in Node.js. Loosely based on Rails' ActiveRelation.
+
+## Getting Started
+Install the module with: `npm install db-query`
+
+```javascript
+var query = require('db-query');
+query.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Mark Selby  
+Licensed under the MIT license.
---
+++ test/db-query_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var query = require('../lib/query.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['query'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal('query', 'query', 'should be a query.');
+    test.done();
+  },
+};
